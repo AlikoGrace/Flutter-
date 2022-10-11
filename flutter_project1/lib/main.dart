d5080650aaf81f5952c564dece3cfdef0ae84002
AlikoGrace <gracealiko09@gmail.com>
2022-10-11 02:49:48 +0000

    first flutter project

---
+++ flutter_project1/lib/main.dart
@@ -0,0 +1,25 @@
+
+
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(
+      MaterialApp(
+        home: Scaffold(
+          appBar: AppBar(title: Text("welcome"),),
+      body:Center(
+        child: Container(
+          width: 360,
+          color: Colors.red[700],
+          child: Text(
+              "Hello world",
+            style: TextStyle(fontSize: 60,
+            color: Colors.white),
+
+          ),
+        ),
+      ),
+  ),
+  ));
+}
+
